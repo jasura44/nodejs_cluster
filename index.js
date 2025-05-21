ff18abac8b3bc99b1d3292c545c68c2a15d95aa5
jasura44 <gunasagaran.suresh@gmail.com>
2025-05-21 11:42:29 +0800

    Added code

---
+++ index.js
@@ -0,0 +1,29 @@
+const cluster = require('cluster');
+const http = require('http');
+const os = require('os');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+    // Master process
+    console.log(`Master process ${process.pid} is running`);
+
+    // Fork workers based on CPU cores
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    // Log when a worker dies
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    // Worker processes
+      // Worker processes run the HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end(`Hello from worker ${process.pid}\n`);
+  }).listen(3000, () => {
+    console.log(`Worker process ID: ${process.pid} is listening on port 3000`);
+  });
+    console.log(`Worker ${process.pid} started`);
+}
